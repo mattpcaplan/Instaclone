0f3d4051287c0a374bec889ea3602bf7868c91cf
Matt Caplan <mattpcaplan@gmail.com>
2017-05-02 12:15:44 -0400

    added in the show/create.  Also had to install ImageMagick

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,5 +7,23 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
+
+  def create
+    @post = Post.new(allow_post)
+    if @post.save
+      flash[:submitted] = "Submitted!"
+      redirect_to post_path(@post)
+    else
+      flash[:error] = @post.errors.full_messages
+      redirect_to new_post_path
+  end
+end
+
+
+  private
+    def allow_post
+      params.require(:post).permit(:image, :description)
+    end
 end
